d8b808eeb16e84c4ba2477f9a5dd9e589109c806
troyready <troy@troyready.com>
2014-08-29 19:19:45 -0700

    add simple platform guard to attributes

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'jared.king@biola.edu'
 license          'Apache 2.0'
 description      'Upgrades chef-client on Windows systems'
 long_description 'Upgrades chef-client on Windows systems'
-version          '1.0.2'
+version          '1.0.3'
 
